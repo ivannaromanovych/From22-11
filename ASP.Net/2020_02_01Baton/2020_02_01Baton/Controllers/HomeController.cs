95242a48430b65f9dc296ef264cc49678d61b964
Nirvana <ivankaromanovych12@gmail.com>
2020-02-01 15:01:03 +0200

    It's asp.net, we added js, css and fonts folders; made a simple project

---
+++ ASP.Net/2020_02_01Baton/2020_02_01Baton/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2020_02_01Baton.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string About()
+        {
+            return "This is method about";
+        }
+    }
+}
---
+++ ASP.Net/2020_02_01Baton/2020_02_01Baton/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2020_02_01Baton.MvcApplication" Language="C#" %>

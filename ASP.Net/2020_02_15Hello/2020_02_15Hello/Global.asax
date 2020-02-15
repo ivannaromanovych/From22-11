2281401c355b34708a7dff2635cbe84ad64e4094
Nirvana <ivankaromanovych12@gmail.com>
2020-02-15 14:30:10 +0200

    initialized new asp.net-project

---
+++ ASP.Net/2020_02_15Hello/2020_02_15Hello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2020_02_15Hello.MvcApplication" Language="C#" %>
---
+++ ASP.Net/2020_02_15Hello/2020_02_15Hello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2020_02_15Hello.Startup))]
+namespace _2020_02_15Hello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
